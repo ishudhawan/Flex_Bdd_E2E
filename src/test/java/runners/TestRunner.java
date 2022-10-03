f9ace49e899396556f4326ce1daa6f8ba591d091
idhawan <idhawan@twilio.com>
2022-10-03 18:24:33 +0530

    Report portal addition

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,9 +1,10 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.*;
-
+@RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty",
                 "html:target/cucumber-reports/cucumber-pretty",
@@ -14,7 +15,7 @@ import io.cucumber.testng.*;
         tags = "@api",
         monochrome = true)
 
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class TestRunner{
 //    public TestNGCucumberRunner testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
 //        @AfterClass(alwaysRun = true)
 //        public void tearDownClass() {
